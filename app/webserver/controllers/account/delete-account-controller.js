f73dac7650b4441b7a1a34d96c8c0f67f16b7b26
Alex <alex.velo.santorum@gmail.com>
2020-01-16 18:14:45 +0100

    Añadido controlador delete user

---
+++ app/webserver/controllers/account/delete-account-controller.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const Joi = require('@hapi/joi');
+const mysqlPool = require('../../../database/mysql-pool');
+
+async function validate(data){
+    const schema = Joi.object({
+      userId: Joi.string()
+      .guid({
+        version: ["uuidv4"]
+      })
+      .required()
+    })
+}
+
+async function deleteAccount(req, res, next){
+  const { userId } = req.claims;
+  const accountData = { userId }
+
+  try{
+    await validate(accountData)
+  } catch (e) {
+    console.error(e);
+    return res.status(400).send(e);
+  }
+
+  let connection;
+  try{
+    connection = await mysqlPool.getConnection();
+    const sqlQuery = `DELETE FROM user
+      WHERE id = ?`;
+
+    const [deletedStatus] = await connection.execute(sqlQuery, [userId]);
+    connection.release();
+    if (deletedStatus.affectedRows !== 1) {
+      return res.status(404).send();
+    }
+
+    return res.status(204).send();
+
+  } catch (e) {
+    if (connection) {
+      connection.release();
+    }
+
+    return res.status(500).send(e.message);
+  }
+}
+
+module.exports = deleteAccount;
--- app/webserver/routes/account-router.js
+++ app/webserver/routes/account-router.js
@@ -5,11 +5,13 @@ const checkAccountSession = require("../controllers/account/check-account-sessio
 const createAccount = require("../controllers/account/create-account-controller");
 const getAccount = require("../controllers/account/get-account-controller");
 const updateAccount = require("../controllers/account/update-account-controller");
+const deleteAccount = require("../controllers/account/delete-account-controller");
 
 const router = express.Router();
 
 router.post("/account", createAccount);
 router.get("/account", checkAccountSession, getAccount);
 router.put("/account", checkAccountSession, updateAccount);
+router.delete("/account", checkAccountSession, deleteAccount);
 
 module.exports = router;
