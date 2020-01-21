47851216294a76754d4b0a58d054091e2c5b6664
david <d5g6f9@gmail.com>
2020-01-21 18:08:00 +0100

    crete,get y get all de themes

--- app/webserver/index.js
+++ app/webserver/index.js
@@ -1,21 +1,23 @@
-"use strict";
+'use strict';
 
-const express = require("express");
+const express = require('express');
 
-const accountRouter = require("./routes/account-router");
-const authRouter = require("./routes/auth-router");
-const messageRouter = require("./routes/message-router");
-const projectRouter = require("./routes/project-router");
-const userRouter = require("./routes/user-router");
+const accountRouter = require('./routes/account-router');
+const authRouter = require('./routes/auth-router');
+const messageRouter = require('./routes/message-router');
+const projectRouter = require('./routes/project-router');
+const userRouter = require('./routes/user-router');
+const themeRouter = require('./routes/theme-router');
 
 const app = express();
 
 app.use(express.json());
-app.use("/api", accountRouter);
-app.use("/api", authRouter);
-app.use("/api", messageRouter);
-app.use("/api", projectRouter);
-app.use("/api", userRouter);
+app.use('/api', accountRouter);
+app.use('/api', authRouter);
+app.use('/api', messageRouter);
+app.use('/api', projectRouter);
+app.use('/api', userRouter);
+app.use('/api', themeRouter);
 
 let server = null;
 async function listen(port) {
@@ -37,7 +39,7 @@ async function close() {
     await server.close();
     server = null;
   } else {
-    console.error("Can not close a non started server");
+    console.error('Can not close a non started server');
   }
 }
 
