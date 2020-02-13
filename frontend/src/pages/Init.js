81d912f08ab190676fe1120625b72fb6f8ec866c
david <d5g6f9@gmail.com>
2020-02-13 11:59:42 +0100

    init

--- frontend/src/pages/Init.js
+++ frontend/src/pages/Init.js
@@ -13,12 +13,123 @@ export function Init() {
   let history = useHistory();
   const { setRole, setCurrentUser } = useAuth();
 
-
-return (
+  return (
     <div>
       <div>
         <Navbar />
       </div>
-    </div>  
-);
+
+      <div class="bg-cover bg-center min-h-screen backimageinit">
+        <div>
+          <div class="slideshow-container">
+            <div class="mySlides fade">
+              <div class="numbertext">1 / 3</div>
+              <img class="w-full" src="../image/init1.jpg" /*style="width:100%" */ />
+              <div class="text">Caption Text</div>
+            </div>
+
+            <div class="mySlides fade">
+              <div class="numbertext">2 / 3</div>
+              <img class="w-full" src="../image/init2.jpg" /*style="width:100%" */ />
+              <div class="text">Caption Two</div>
+            </div>
+
+            <div class="mySlides fade">
+              <div class="numbertext">3 / 3</div>
+              <img class="w-full" src="../image/init3.jpg" /*style="width:100%"*/ />
+              <div class="text">Caption Three</div>
+            </div>
+
+            <a class="prev" onclick="plusSlides(-1)">
+              &#10094;
+            </a>
+            <a class="next" onclick="plusSlides(1)">
+              &#10095;
+            </a>
+          </div>
+          <br />
+
+          <div /*style="text-align:center"*/>
+            <span class="dot" onclick="currentSlide(1)"></span>
+            <span class="dot" onclick="currentSlide(2)"></span>
+            <span class="dot" onclick="currentSlide(3)"></span>
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-10">
+        <div class="px-10">
+          <div class="block md:flex justify-between md:-mx-2">
+            <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+              <div class="bg-white rounded-lg overflow-hidden shadow relative">
+                <img
+                  class="h-56 w-full object-cover object-center"
+                  src="https://images.unsplash.com/photo-1457282367193-e3b79e38f207?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1654&q=80"
+                  alt=""
+                />
+                <div class="p-4 h-auto md:h-40 lg:h-48">
+                  <a
+                    href="#"
+                    class="block text-blue-500 hover:text-blue-600 font-semibold mb-2 text-lg md:text-base lg:text-lg"
+                  >
+                    Woman standing under blue sky
+                  </a>
+                  <div class="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quidem blanditiis unde asperiores?
+                    Officia amet perspiciatis ad quibusdam incidunt eaque, nobis, eveniet neque porro id commodi
+                    quisquam debitis!
+                  </div>
+                  <div class="relative mt-2 lg:absolute bottom-0 mb-4 md:hidden lg:block"></div>
+                </div>
+              </div>
+            </div>
+            <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+              <div class="bg-white rounded-lg overflow-hidden shadow relative">
+                <img
+                  class="h-56 w-full object-cover object-center"
+                  src="https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                  alt=""
+                />
+                <div class="p-4 h-auto md:h-40 lg:h-48">
+                  <a
+                    href="#"
+                    class="block text-blue-500 hover:text-blue-600 font-semibold mb-2 text-lg md:text-base lg:text-lg"
+                  >
+                    Woman walking on pathway
+                  </a>
+                  <div class="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure aut quia alias ullam eveniet sunt!
+                    Ipsa, sunt. Inventore ipsum sit quasi. Alias quasi officiis blanditiis!
+                  </div>
+                  <div class="relative mt-2 lg:absolute bottom-0 mb-4 md:hidden lg:block"></div>
+                </div>
+              </div>
+            </div>
+            <div class="w-full lg:w-1/3 md:mx-2 mb-4 md:mb-0">
+              <div class="bg-white rounded-lg overflow-hidden shadow relative">
+                <img
+                  class="h-56 w-full object-cover object-center"
+                  src="https://images.unsplash.com/photo-1467238307002-480ffdd260f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                  alt=""
+                />
+                <div class="p-4 h-auto md:h-40 lg:h-48">
+                  <a
+                    href="#"
+                    class="block text-blue-500 hover:text-blue-600 font-semibold mb-2 text-lg md:text-base lg:text-lg"
+                  >
+                    Walking through a forest in the afternoon
+                  </a>
+                  <div class="text-gray-600 text-sm leading-relaxed block md:text-xs lg:text-sm">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi nemo magni saepe cumque error quia
+                    quae sint ducimus, maiores doloremque.
+                  </div>
+                  <div class="relative mt-2 lg:absolute bottom-0 mb-4 md:hidden lg:block"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
