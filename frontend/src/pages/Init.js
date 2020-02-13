476e75a37def0231d0c91736969c7e786b86c5c2
david <d5g6f9@gmail.com>
2020-02-13 12:06:02 +0100

    init

--- frontend/src/pages/Init.js
+++ frontend/src/pages/Init.js
@@ -21,40 +21,7 @@ export function Init() {
 
       <div class="bg-cover bg-center min-h-screen backimageinit">
         <div>
-          <div class="slideshow-container">
-            <div class="mySlides fade">
-              <div class="numbertext">1 / 3</div>
-              <img class="w-full" src="../image/init1.jpg" /*style="width:100%" */ />
-              <div class="text">Caption Text</div>
-            </div>
-
-            <div class="mySlides fade">
-              <div class="numbertext">2 / 3</div>
-              <img class="w-full" src="../image/init2.jpg" /*style="width:100%" */ />
-              <div class="text">Caption Two</div>
-            </div>
-
-            <div class="mySlides fade">
-              <div class="numbertext">3 / 3</div>
-              <img class="w-full" src="../image/init3.jpg" /*style="width:100%"*/ />
-              <div class="text">Caption Three</div>
-            </div>
-
-            <a class="prev" onclick="plusSlides(-1)">
-              &#10094;
-            </a>
-            <a class="next" onclick="plusSlides(1)">
-              &#10095;
-            </a>
-          </div>
-          <br />
-
-          <div /*style="text-align:center"*/>
-            <span class="dot" onclick="currentSlide(1)"></span>
-            <span class="dot" onclick="currentSlide(2)"></span>
-            <span class="dot" onclick="currentSlide(3)"></span>
-          </div>
-        </div>
+          
       </div>
 
       <div class="mt-10">
