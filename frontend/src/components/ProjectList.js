e99cc2eb8470d8c2db5172d73eb0863c93c60709
david <d5g6f9@gmail.com>
2020-02-17 21:35:13 +0100

    projects en init

--- frontend/src/components/ProjectList.js
+++ frontend/src/components/ProjectList.js
@@ -48,11 +48,7 @@ export function ProjectList({ projects }) {
                         to="/user"
                         className="mt-6 block h-12 w-12 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white"
                       >
-                        <img
-                          className="h-full w-full object-cover"
-                          src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80"
-                          alt="Your avatar"
-                        />
+                        <img className="h-full w-full object-cover" src={role.avatar_url} alt="Your avatar" />
                       </Link>
                       <div className="text-sm ml-4 pt-6">
                         <p className="text-gray-900 leading-none">{project.name + ' ' + project.first_name}</p>
